f67c203a41ec3fac3dcd613a39b9a6d059ba789b
Artem <erestocorp@mail.ru>
2021-05-29 22:33:26 +0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public Vector3 direction;
+
+    Rigidbody body;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        body.MovePosition(body.position + direction * Time.deltaTime);
+    }
+}
